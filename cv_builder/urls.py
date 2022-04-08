d1bcac082851ead31232ccb32373a3fdbfa0dc05
adridot <adrien.didot@gmail.com>
2022-04-08 14:12:15 +0300

    Template + export pdf

--- cv_builder/urls.py
+++ cv_builder/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('display_cv', views.display_cv, name='display_cv'),
+    path('export_to_pdf', views.export_to_pdf, name='export_to_pdf'),
     path('admin/', admin.site.urls),
 ]
--- cv_builder/views.py
+++ cv_builder/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, FileResponse
+from cv_builder.pdf_export import get_pdf_from_html
 
 
 def index(request):
@@ -9,3 +9,15 @@ def index(request):
 
 def display_cv(request):
     return render(request, 'cv_builder/cv.html')
+
+
+def export_to_pdf(request):
+    url = request.build_absolute_uri('/display_cv')
+    result = get_pdf_from_html(url)
+    with open('media/exported_cv/cv.pdf', 'wb') as pdf_file:
+        pdf_file.write(result)
+    with open('media/exported_cv/cv.pdf', 'rb') as pdf_file:
+        response = HttpResponse(pdf_file, content_type='application/pdf')
+        #TODO: add filename
+        response['Content-Disposition'] = 'filename="home_page.pdf"'
+    return response
